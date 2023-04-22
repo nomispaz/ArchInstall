1b333e563571165fdc2d798a72f34fcf2382d8ef
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-22 07:27:26 +0200

    Update 2_install.sh

--- 2_install.sh
+++ 2_install.sh
@@ -25,7 +25,7 @@ sudo pacman --noconfirm -S archlinux-keyring
 #kde with amd igpu
 pacman -Syu --noconfirm acpi acpi_call acpid alsa-firmware alsa-plugins alsa-utils \
 apparmor ark bluez bluez-utils clamav cups exfatprogs file-roller firefox firewalld \
-ghostscript hplip hwdetect hwinfo iptables-nft ipw2100-fw ipw2200-fw man nfs-utils \
+ghostscript hplip hwdetect hwinfo iptables-nft man nfs-utils \
 ntfs-3g os-prober pipewire pipewire-alsa dkms xf86-video-amdgpu  \
 pipewire-jack pipewire-pulse rkhunter rsync unrar unzip wget wpa_supplicant xdg-desktop-portal \
 xorg-server xorg-xdpyinfo xorg-xinput xorg-xkill xorg-xlsclients xorg-xrandr \
