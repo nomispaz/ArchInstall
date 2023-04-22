21c7121eab7d609975124ce0ab41364b924decd5
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-22 11:18:25 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,92 @@
-pacman -Syu --noconfirm acpi acpi_call acpid alsa-firmware alsa-plugins alsa-utils \
-apparmor ark bluez bluez-utils clamav cups exfatprogs file-roller firefox firewalld \
-ghostscript hplip hwdetect hwinfo iptables-nft ipw2100-fw ipw2200-fw man nfs-utils \
-ntfs-3g os-prober pipewire pipewire-alsa dkms xf86-video-amdgpu  \
-pipewire-jack pipewire-pulse rkhunter rsync unrar unzip wget wpa_supplicant xdg-desktop-portal \
-xorg-server xorg-xdpyinfo xorg-xinput xorg-xkill xorg-xlsclients xorg-xrandr \
-zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting \
+pacstrap /mnt \
+base \
+base-devel \
+btrfs-progs \
+efibootmgr \
+git \
+grep \
+grub \
+intel-ucode \
+amd-ucode \
+nano \
+vim \
+openssh \
+htop \
+wget \
+sudo \
+xdg-user-dirs \
+xdg-utils \
+xf86-input-synaptics \
+archlinux-keyring \
+pacman-contrib \
+iwd \
+networkmanager \
+wireless_tools \
+wpa_supplicant \
+smartmontools \
+xdg-utils \
+pipewire \
+pipewire-alsa \
+pipewire-jack \
+pipewire-pulse \
+gst-plugin-pipewire \
+libpulse \
+wireplumber \
+linux \
+linux-firmware \
+linux-headers \
+linux-zen \
+linux-zen-headers \
+xorg-server \
+xorg-xinit \
+plasma-meta \
+konsole \
+kwrite \
+dolphin \
+ark \
+plasma-wayland-session \
+egl-wayland \
+acpi \
+acpi_call \
+acpid \
+alsa-firmware \
+alsa-plugins \
+alsa-utils \
+apparmor \
+ark \
+bluez \
+bluez-utils \
+clamav \
+cups \
+exfatprogs \
+file-roller \
+firefox \
+firewalld \
+ghostscript \
+hplip \
+hwdetect \
+hwinfo \
+man \
+nfs-utils \
+ntfs-3g \
+os-prober \
+dkms \
+xf86-video-amdgpu  \
+rkhunter \
+rsync \
+unrar \
+unzip \
+wpa_supplicant \
+xdg-desktop-portal \
+xorg-server \
+xorg-xdpyinfo \
+xorg-xinput \
+xorg-xkill \
+xorg-xlsclients \
+xorg-xrandr \
+zsh \
+zsh-autosuggestions \
+zsh-completions zsh-history-substring-search zsh-syntax-highlighting \
 calibre chromium clipgrab discord keepassxc kompare notepadqq obs-studio qt5-wayland spectacle \
 thunderbird veracrypt vlc breeze-gtk dolphin kate kcalc kde-gtk-config khotkeys kinfocenter kinit \
 konsole kscreen partitionmanager plasma-desktop plasma-disks plasma-nm plasma-pa plasma-systemmonitor \
