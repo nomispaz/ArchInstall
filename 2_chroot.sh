45dc334ec74250a4b72c2faa9b823f3b82c698fa
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-22 16:52:19 +0200

    Update and rename 2_install.sh to 2_chroot.sh

--- 2_chroot.sh
+++ 2_chroot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# install.sh
+# 2_chroot.sh
 #
-# Script by LordPazifist
+# Script by nomispaz
 # Version 1
-# Date 20.04.2023
+# Date 22.04.2023
 
 echo "set locales and time"
 
@@ -22,21 +22,21 @@ locale-gen
 echo "[multilib]" | tee -a /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" | tee -a /etc/pacman.conf
 
-#nvidia
+echo "Install nvidia-open"
 pacman -Syu --noconfirm nvidia-open-dkms nvidia-utils nvidia-settings nvidia-prime lib32-nvidia-utils
 
-#gaming
+echo "Install gaming-tools"
 pacman -Syu --noconfirm vulkan-icd-loader lib32-vulkan-icd-loader \
 gamescope gamemode steam lutris wine lib32-gnutls libretro-mgba wine-mono wine-gecko winetricks
 
-#additional qemu
+echo "install qemu and libvirt"
 pacman -Syu --noconfirm virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq
 
 echo "install grub"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
 
 #echo "add nvidia-drm.modeset=1 and uncomment GRUB_DISABLE_OS_PROBER
-#nano /etc/default/grub
+echo 'GRUB_DISABLE_OS_PROBER="false"' >> /etc/default/grub
 
 echo "generate grub"
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -57,9 +57,9 @@ firewall-cmd --set-default-zone block
 echo "set root password"
 passwd
 
+echo "create user and set password"
 useradd -m simonheise
 usermod -aG sys,wheel,users,rfkill,simonheise,libvirt simonheise
-passwd
 passwd simonheise
 
 echo "Defaults targetpw # Ask for the password of the target user" >> /etc/sudoers
