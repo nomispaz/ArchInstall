7b57a0039bc9f834a5e5945d9967b03bb8f9c3c4
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-22 07:24:24 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+pacman -Syu --noconfirm acpi acpi_call acpid alsa-firmware alsa-plugins alsa-utils \
+apparmor ark bluez bluez-utils clamav cups exfatprogs file-roller firefox firewalld \
+ghostscript hplip hwdetect hwinfo iptables-nft ipw2100-fw ipw2200-fw man nfs-utils \
+ntfs-3g os-prober pipewire pipewire-alsa dkms xf86-video-amdgpu  \
+pipewire-jack pipewire-pulse rkhunter rsync unrar unzip wget wpa_supplicant xdg-desktop-portal \
+xorg-server xorg-xdpyinfo xorg-xinput xorg-xkill xorg-xlsclients xorg-xrandr \
+zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting \
+calibre chromium clipgrab discord keepassxc kompare notepadqq obs-studio qt5-wayland spectacle \
+thunderbird veracrypt vlc breeze-gtk dolphin kate kcalc kde-gtk-config khotkeys kinfocenter kinit \
+konsole kscreen partitionmanager plasma-desktop plasma-disks plasma-nm plasma-pa plasma-systemmonitor \
+plasma-wayland-session powerdevil sddm-kcm screenfetch base plasma
