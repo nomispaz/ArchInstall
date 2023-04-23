7315cad796afed3dfe4a856ab8abdf439e9f805c
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-23 12:25:44 +0200

    Update dracut-install.sh

--- usr/local/bin/dracut-install.sh
+++ usr/local/bin/dracut-install.sh
@@ -2,14 +2,42 @@
 
 args=('--force' '--no-hostonly-cmdline')
 
-while read -r line; do
-	if [[ "$line" == 'usr/lib/modules/'+([^/])'/pkgbase' ]]; then
-		read -r pkgbase < "/${line}"
-		kver="${line#'usr/lib/modules/'}"
-		kver="${kver%'/pkgbase'}"
+all=0
+lines=()
 
-		install -Dm0644 "/${line%'/pkgbase'}/vmlinuz" "/boot/vmlinuz-${pkgbase}"
-		dracut "${args[@]}" --hostonly "/boot/initramfs-${pkgbase}.img" --kver "$kver"
-		dracut "${args[@]}" --no-hostonly "/boot/initramfs-${pkgbase}-fallback.img" --kver "$kver"
+if (( all )); then
+	lines=(/usr/lib/modules/*)
+fi
+
+while read -r line; do
+	if [[ "${line}" != */vmlinuz ]]; then
+		# triggers when it's a change to dracut files
+		all=1
+		continue
+	fi
+
+	lines+=("/${line%/vmlinuz}")
+
+	pkgbase="$(<"${lines[-1]}/pkgbase")"
+	install -Dm644 "/${line}" "/boot/vmlinuz-${pkgbase}"
+done
+
+for line in "${lines[@]}"; do
+	if ! pacman -Qqo "${line}/pkgbase" &> /dev/null; then
+		# if pkgbase does not belong to any package then skip this kernel
+		continue
+	fi
+
+	pkgbase="$(<"${line}/pkgbase")"
+	kver="${line##*/}"
+	dracut_restore_img="/usr/lib/modules/${kver}/initrd"
+
+	echo ":: Building initramfs for ${pkgbase} (${kver})"
+	dracut --force --hostonly --no-hostonly-cmdline${DRACUT_EXTRA_PARAMS} ${dracut_restore_img} "${kver}"
+	install -Dm644 ${dracut_restore_img} "/boot/initramfs-${pkgbase}.img"
+
+    if [[ ${NO_DRACUT_FALLBACK} != "true" ]] ; then
+		echo ":: Building fallback initramfs for ${pkgbase} (${kver})"
+		dracut --force --no-hostonly${DRACUT_EXTRA_PARAMS} "/boot/initramfs-${pkgbase}-fallback.img" "${kver}"
 	fi
 done
