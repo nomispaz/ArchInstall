358a1f9edd185640584641606e28ffbbc69af774
nomispaz <111981114+nomispaz@users.noreply.github.com>
2024-05-14 21:44:43 +0200

    Add files via upload

--- 4_setup_environment.sh
+++ 4_setup_environment.sh
@@ -30,3 +30,15 @@ sed -i 's/#SHA_CRYPT_MAX_ROUNDS 5000/SHA_CRYPT_MAX_ROUNDS 500000/g' /etc/login.d
 echo "predefine host-file for localhost"
 echo "127.0.0.1 localhost" | tee -a /etc/hosts
 echo "127.0.0.1 XMGneo15Arch" | tee -a /etc/hosts
+
+echo "install grub"
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
+
+#echo "add nvidia-drm.modeset=1 and uncomment GRUB_DISABLE_OS_PROBER
+echo 'GRUB_DISABLE_OS_PROBER="false"' >> /etc/default/grub
+
+echo "set kernel parameter"
+sed -i 's/quiet/quiet mitigations=auto security=apparmor amd_pstate=passive nvidia_drm.modeset=1/g' /etc/default/grub
+
+echo "generate grub"
+grub-mkconfig -o /boot/grub/grub.cfg
