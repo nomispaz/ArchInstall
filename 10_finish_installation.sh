5cb0991a4f18584dbf7214990265bdc44cb92349
nomispaz <111981114+nomispaz@users.noreply.github.com>
2024-05-15 10:19:47 +0200

    Add files via upload

---
+++ 10_finish_installation.sh
@@ -0,0 +1,31 @@
+echo "enable services"
+systemctl enable NetworkManager.service
+systemctl enable bluetooth.service
+systemctl enable cups.service
+#hardening --> don't enable sshd
+#systemctl enable sshd.service
+systemctl enable avahi-daemon.service
+systemctl enable libvirtd.service
+systemctl enable firewalld.service
+systemctl enable acpid.service
+systemctl enable sddm.service
+systemctl enable apparmor.service
+systemctl enable clamav-daemon.service
+systemctl enable nvidia-powerd.service
+systemctl enable chronyd.service
+systemctl enable clamav-freshclam.service
+
+firewall-cmd --set-default-zone block
+
+echo "set root password"
+passwd
+
+echo "create user and set password"
+echo "Enter username: "
+read user
+useradd -m --create-home $user
+usermod -aG sys,wheel,users,rfkill,$user,libvirt $user
+passwd $user
+
+echo "Defaults targetpw # Ask for the password of the target user" >> /etc/sudoers
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
